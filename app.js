44062cfbadc6bf2e23118f01552a196099282557
Lukas Swenson <lukas@memoriapress.com>
2022-07-11 21:03:38 -0400

    Add JS sandbox elements

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-let message = "hello world";
+// let person = {
+//     firstName: 'John',
+//     lastName: 'Adams'
+// };
+// showMessage(person.firstName);
+// console.log(person);
 
-showMessage(message);
+// the Termary Operator 
+// let price = 2;
+
+// (price > 10) ? showMessage('yes') : showMessage('no');
+
+
+// Else If statements
+// if (price > 10) { 
+//     showMessage('true');
+// }
+
+// else if (price < 5) {
+//     showMessage('false');
+// }
+
+// for Loop
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+let i = 4
+while (i > 0) {
+    console.log(i);
+    i--;
+}
 
