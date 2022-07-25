998e2529dc642ebfbcbb95b1ec1e3bc0a85aa85c
Lukas Swenson <swenson.lukas1@gmail.com>
2022-07-25 17:30:22 -0400

    Fix email verification notification.

--- app.js
+++ app.js
@@ -1,60 +1,10 @@
-// let person = {
-//     firstName: 'John',
-//     lastName: 'Adams'
-// };
-// showMessage(person.firstName);
-// console.log(person);
 
-// the Termary Operator 
-// let price = 2;
-
-// (price > 10) ? showMessage('yes') : showMessage('no');
-
-
-// Else If statements
-// if (price > 10) { 
-//     showMessage('true');
-// }
-
-// else if (price < 5) {
-//     showMessage('false');
-// }
-
-// for Loop
-// for (let i = 0; i < 10; i++) {
-//     console.log(i);
-// }
-
-// let i = 4
-// while (i > 0) {
-//     console.log(i);
-//     i--;
-// }
-
-// function showMessage(message) {
-//     document.getElementById('message').textContent = message;
-// }
-
-
-// function toggleMenu() {
-//   document.getElementById("mainNavJs").classList.toggle("hide");
-// }
-
-// let x =document.getElementById('hamburgerMenu');
-// x.onclick = toggleMenu;
-
-// function myFunction(x) {
-//   x.classList.toggle("change");
-// }
-
-// On mobile, hamburger menu appears
-// Nav needs to disappear 
-// On Click, hamnurger menu expands to show nav 
-// If menu is open, click x to close page
-
+// Hamburger menu functionality
 document.querySelector('#hamburgerMenu').addEventListener('click', () => {
+
     const menu = document.querySelector('#headerNav');
     var menuIsOpen = menu.style.display === 'flex';
+
   if (menuIsOpen) {
     document.querySelector('#hamburgerMenu').classList.remove('change');
     menu.style.display = 'none';
@@ -64,37 +14,27 @@ document.querySelector('#hamburgerMenu').addEventListener('click', () => {
     document.querySelector('#hamburgerMenu').classList.add('change');
     menu.style.display = 'flex';
   }
+
 });
 
+// Email verification and contact form greeting
+var submitButton = document.getElementById('btn');
 
+submitButton.addEventListener("click", function(){
 
-document.getElementById('btn').addEventListener("click", ValidateEmail);
+  var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-function ValidateEmail(){
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(email.value.match(mailformat))
-{
-// alert("Valid email address!");
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-// document.email.focus();
-return false;
-}
+  if(email.value.match(mailFormat)){
+    let name = fullName.value
+    alert ("Thank you " + name + " for registering! This form is for demonstration purposes only.")
+  }
 
+  else {
+    alert("You have entered an invalid email address!");
+  }
 
-};
+});
 
-if (ValidateEmail() === true) {
-
-function displayName() {
-  let name = fullName.value
-  alert ("Thank you " + name + " for registering! This form is for demonstration purposes only.")
-};
-
-};
 
 
 
